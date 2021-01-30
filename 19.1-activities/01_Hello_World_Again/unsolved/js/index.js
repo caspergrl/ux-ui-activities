658eec84303187ad8cf4f85743e1dc6a4ce24396
Holly Reynolds <iam@Hollyreynolds.net>
2021-01-30 09:52:48 -0500

    test

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,5 @@
+// Create a variable below
+var favDrink="tequila";
+
+// Console.log() your variable here.
+console.log(favDrink);
---
+++ 19.1-activities/02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,18 @@
+// Create the addNumbers function here.
+function addNumbers() {
+    var two=2;
+    var three=3;
+    var five=two + three;
+    console.log(five);
+}
+addNumbers();
+
+// Create the stringConcat function here.
+function strongConcat() {
+    var taco="Hi, my name is";
+    var cat=" Holly";
+    var tacoCat=taco + cat;
+    alert(tacoCat);
+}
+
+strongConcat();
